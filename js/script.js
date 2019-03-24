4452f7fcb628e74143f1e66b78b4bd47d8f534fc
Douglas Hill <dohill06@gmail.com>
2019-03-24 16:13:06 -0700

    Hide t-shirt color menu on load-in

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 $('#name').focus();
 // hide other-title job role input on load-in
 $('#other-title').hide();
+// hide t-shirt 'Color' menu on load-in
+$('#colors-js-puns').hide();
 // show other-title job role input when other is selected
 $('#title').on('change', function() {
     let value = $('#title').val();
