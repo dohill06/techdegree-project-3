6face64a7fe416e1a4b0a04102798b9516d6ccd0
Douglas Hill <dohill06@gmail.com>
2019-03-24 15:50:30 -0700

    Hide 'Other' job role input on load and show on selection

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 $('#name').focus();
+
+$('#other-title').hide();
+
+$('#title').on('change', function() {
+    let value = $('#title').val();
+    if (value === 'other') {
+        $('#other-title').show();
+    } else {
+        $('#other-title').hide(); 
+    }
+});
