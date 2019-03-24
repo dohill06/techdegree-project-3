0d1ff87343ce5b73f336811d8b2fd9577fbabef1
Douglas Hill <dohill06@gmail.com>
2019-03-24 15:55:14 -0700

    Added notes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
+// added focus to name input on load-in
 $('#name').focus();
-
+// hide other-title job role input on load-in
 $('#other-title').hide();
-
+// show other-title job role input when other is selected
 $('#title').on('change', function() {
     let value = $('#title').val();
     if (value === 'other') {
