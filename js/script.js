38ebc2d187fe88a8050688ba094b3916ed331037
Douglas Hill <dohill06@gmail.com>
2019-03-26 15:37:29 -0700

    Made 'Color' menu dynamic!

--- js/script.js
+++ js/script.js
@@ -16,8 +16,8 @@ $('#title').on('change', function() {
         $('#other-title').hide(); 
     }
 });
-
-
+// variable for regex check of t-shirts
+const shirtRegEx = new RegExp('P');
 // make color menu show on change with correct options
 $('#design').on('change', function() {
     $('#design option').eq(0).hide();
@@ -25,16 +25,38 @@ $('#design').on('change', function() {
     $('#color').show();
 
     if ($(this).val() == 'js puns') {
-        console.log('yes');
-        $('#color option').slice(3).hide();
-        $('#color option').slice(0, 3).show();
-        $('#color option').eq(0).attr('selected', true);
-        $('#color option').eq(3).attr('selected', false);
+        let optNotSelected = '';
+        let optSelected = '';
+        $('#color option').each(function(i, el) {
+            if (shirtRegEx.test($(el).text())) {
+                $(el).show();
+                optNotSelected = (i + 1);
+            } else {
+                $(el).hide();
+                optSelected = (i - i);
+            }
+        });
+        $('#color option').eq(optNotSelected).attr('selected', false);
+        $('#color option').eq(optSelected).attr('selected', true);
     } else if ($(this).val() == 'heart js') {
-        console.log('no');
-        $('#color option').slice(0, 3).hide();
-        $('#color option').slice(3).show();
-        $('#color option').eq(3).attr('selected', true);
-        $('#color option').eq(0).attr('selected', false);
+        $('#color option').each(function (i, el) {
+            if (shirtRegEx.test($(el).text()) == false) {
+                $(el).show();
+                optNotSelected = (i - i);
+            } else {
+                $(el).hide();
+                optSelected = (i + 1);
+            }
+        });
+        $('#color option').eq(optNotSelected).attr('selected', false);
+        $('#color option').eq(optSelected).attr('selected', true);
     }
-})
+});
+
+$('.activities input').on('click', function() {
+    if ($(this).prop('checked')) {
+        console.log('check');
+    } else if ($(this).prop('checked') == false) {
+        console.log('unchecked');
+    }
+});
