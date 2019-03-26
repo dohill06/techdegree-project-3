052ef899d84ba6a570aa4949d0500079f7938e09
Douglas Hill <dohill06@gmail.com>
2019-03-26 08:45:31 -0700

    Added working 'Color' menu

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@
 $('#name').focus();
 // hide other-title job role input on load-in
 $('#other-title').hide();
-// hide t-shirt 'Color' menu on load-in
-$('#colors-js-puns').hide();
+// hide t-shirt 'Color' menu on load-in and add instructions
+$('#color').hide();
+$('#colors-js-puns label').append('<p>Please select a T-shirt theme.</p>');
+$('#colors-js-puns p').css('margin-top', '11px');
+
 // show other-title job role input when other is selected
 $('#title').on('change', function() {
     let value = $('#title').val();
@@ -13,3 +16,26 @@ $('#title').on('change', function() {
         $('#other-title').hide(); 
     }
 });
+
+
+
+$('#design').on('change', function() {
+    $('#design option').eq(0).hide();
+    $('#colors-js-puns p').hide();
+    $('#color').show();
+
+    if ($(this).val() == 'js puns') {
+        console.log('yes');
+        $('#color option').slice(3).hide();
+        $('#color option').slice(0, 3).show();
+        $('#color option').eq(0).attr('selected', true);
+        $('#color option').eq(3).attr('selected', false);
+    } else if ($(this).val() == 'heart js') {
+        console.log('no');
+        $('#color option').slice(0, 3).hide();
+        $('#color option').slice(3).show();
+        $('#color option').eq(3).attr('selected', true);
+        $('#color option').eq(0).attr('selected', false);
+    }
+    
+})
