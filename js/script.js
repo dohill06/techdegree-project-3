9e9e650ae9cffec466c6ff85af44950ec10fd52f
Douglas Hill <dohill06@gmail.com>
2019-03-29 18:57:12 -0700

    Setting up submit process

--- js/script.js
+++ js/script.js
@@ -13,10 +13,11 @@ $('#colors-js-puns p').css('margin-top', '9px');
 const $creditCard = $('#credit-card')
 const $payPal = $creditCard.next();
 const $bitCoin = $creditCard.next().next();
+$('#payment option').eq(0).hide();
 $payPal.hide();
 $bitCoin.hide();
 $('#payment option').eq(1).attr('selected', true);
-$('#payment option').eq(0).attr('disabled', true);
+
 
 // show other-title job role input when other is selected
 $('#title').on('change', function() {
@@ -151,4 +152,81 @@ $('#payment').on('change', function(){
         $creditCard.hide();
         $payPal.hide();
     }
+});
+
+
+const $emailInput = $('#mail');
+const emailRegEx = /^[^@]+@[^@.]+\.[a-z]{2,}$/i;
+
+const $creditInput = $('#cc-num');
+const creditNumRegEx = /^\d{13,16}$/;
+
+const $creditZipInput = $('#zip');
+const creditZipRegEx = /^\d{5}$/;
+
+const $creditCvvInput = $('#cvv');
+const creditCvvRegEx = /^\d{3}$/;
+
+
+function regExTest(reg, text) {
+    return reg.test(text.val());
+}
+
+
+function errorMessage() {
+
+}
+const span = $('<span> That is not a valid Email address</span>').css('color', 'red');
+$emailInput.prev().append($(span));
+$(span).hide();
+
+// submit button
+$('form').on('submit', function(e) {
+    if ($('#name').val().length == 0) {
+        console.log('c1')
+        e.preventDefault();
+    } 
+    
+    if (regExTest(emailRegEx, $emailInput) == false) {
+        console.log('c2')
+        e.preventDefault();
+    }
+
+    if ($('input:checked').length == 0) {
+        console.log('c3')
+        e.preventDefault();
+    }
+
+    if ($('#payment').val() == 'credit card') {
+        console.log('cc4');
+        if (regExTest(creditNumRegEx, $creditInput) == false) {
+            console.log('cc5');
+            e.preventDefault();
+        }
+        if (regExTest(creditZipRegEx, $creditZipInput) == false) {
+            console.log('cc6');
+            e.preventDefault();
+        }
+        if (regExTest(creditCvvRegEx, $creditCvvInput) == false) {
+            console.log('cc7');
+            e.preventDefault();
+        }
+    }
+});
+
+
+// real-time mail input!
+$emailInput.on('input', function () {
+    if (regExTest(emailRegEx, $emailInput)) {
+        $(span).hide();
+        console.log('yes');
+    } else if ($emailInput.val() == 0) {
+        $(span).hide();
+        console.log('fuck');
+    } else {
+        $(span).show();
+        
+        console.log('no');
+    }
+        
 });
